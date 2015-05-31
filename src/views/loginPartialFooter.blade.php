4a4a80ae8e8796fe873acf85e11deac3abcbfc7a
Jose Galindo (Mac) <jgalindo@cs.com.gt>
2015-05-31 08:02:40 -0600

    Cambio de ruta reset

--- src/views/loginPartialFooter.blade.php
+++ src/views/loginPartialFooter.blade.php
@@ -3,7 +3,7 @@
     <div class="row">
       <div class="col-xs-8 pull-left">
         @if(Config::get('login::olvido.habilitado')) 
-          <small><a href="{{URL::to('reset/create')}}">{{Config::get('login::olvido.titulo')}}</a></small><br>
+          <small><a href="{{URL::to('reset')}}">{{Config::get('login::olvido.titulo')}}</a></small><br>
         @endif
         @if(Config::get('login::registro.habilitado')) 
           <small><a href="{{URL::to('signup')}}">{{Config::get('login::registro.titulo')}}</a></small><br>
