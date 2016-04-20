ba29d9ad190ae5404e55bf2e78d7e09199899747
CSMacbook <jgalindo@cs.com.gt>
2016-04-20 09:12:48 -0600

    Fix a col-sm-6 en boton de login

--- src/views/loginPartialFooter.blade.php
+++ src/views/loginPartialFooter.blade.php
@@ -1,7 +1,7 @@
 @if(Config::get('login::usuario.habilitado') && Config::get('login::password.habilitado'))
   <div class="panel-footer">
     <div class="row">
-      <div class="col-xs-8 pull-left">
+      <div class="col-sm-6">
         @if(Config::get('login::olvido.habilitado')) 
           <small><a href="{{URL::to('reset')}}">{{Config::get('login::olvido.titulo')}}</a></small><br>
         @endif
@@ -9,8 +9,8 @@
           <small><a href="{{URL::to('signup')}}">{{Config::get('login::registro.titulo')}}</a></small><br>
         @endif
       </div>
-      <div class="col-xs-4 pull-right">
-        <button type="submit" class="btn btn-large btn-success btn-block pull-right" autocomplete="off">{{Config::get('login::botonlogin')}}</button>
+      <div class="col-sm-6">
+        <button type="submit" class="btn btn-large btn-success btn-block" autocomplete="off">{{Config::get('login::botonlogin')}}</button>
       </div>
     </div>
   </div>
